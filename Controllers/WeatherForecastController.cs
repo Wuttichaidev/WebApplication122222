85db88e10737a609e21f5da061b857a9bc7eafc6
Wuttichai Siriwat <Wuttichai.s@jaspal.co.th>
2025-09-01 15:05:58 +0700

    test

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -21,9 +21,16 @@ namespace WebApplication122222.Controllers
         [HttpGet(Name = "GetWeatherForecast")]
         public IEnumerable<WeatherForecast> Get()
         {
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            try
             {
-                Date = DateTime.Now.AddDays(index),
+
+            }catch(Exception ex)
+            {
+                _logger.LogError(ex, "An error occurred while getting the weather forecast.");
+            }
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            { 
+            Date = DateTime.Now.AddDays(index),
                 TemperatureC = Random.Shared.Next(-20, 55),
                 Summary = Summaries[Random.Shared.Next(Summaries.Length)]
             })
