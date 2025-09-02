0f644ba14314450271575a5907b27decc0812cec
Wuttichai Siriwat <Wuttichai.s@jaspal.co.th>
2025-09-02 14:20:59 +0700

    123

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -12,6 +12,9 @@ namespace WebApplication122222.Controllers
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
+        private readonly ILogger<WeatherForecastController> _logger;
+
+
         [HttpGet(Name = "GetWeatherForecast")]
         public IEnumerable<WeatherForecast> Get()
         {
