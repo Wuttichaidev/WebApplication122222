baed446c4caab0b6b23d7bb435b2ccb28163d704
Wuttichai Siriwat <Wuttichai.s@jaspal.co.th>
2025-09-02 16:19:42 +0700

    ๅ/-

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -11,12 +11,12 @@ namespace WebApplication122222.Controllers
         {
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
-        private object _logger;
+
+
 
         [HttpGet(Name = "GetWeatherForecast")]
         public IEnumerable<WeatherForecast> Get()
         {
-
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             { 
             Date = DateTime.Now.AddDays(index),
